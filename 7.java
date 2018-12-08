7c4138f59b1ef4dece3a173c5a2afc67b02a01f8
Hemnaath Surya <hemnaathrsurya@gmail.com>
2018-12-09 03:22:43 +0530

    hunter nine prgm

---
+++ 7.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+public class odde{
+     public static void main(String aa[]){
+     int n;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     int a[]=new int[n];
+     for(int i=0;i<n;i++){
+         a[i]=s.nextInt();
+         
+     }
+     for(int i=0;i<n;i++){
+         if(i%2==0){
+             if(a[i]%2!=0){
+                 System.out.print(a[i]);
+             }
+         }
+         else if(i % 2 != 0)
+         {
+             if(a[i]%2==0){
+                 System.out.println(a[i]);
+             }
+         }
+     }
+
+    }
